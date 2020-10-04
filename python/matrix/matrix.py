f3b40f26255a8d0b31f23ef8a167a7aaec8c9fb0
jcabre04 <cabrera.student@gmail.com>
2020-10-04 16:44:08 -0700

    Used list comprehension on Matrix Column function

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,13 +1,10 @@
 class Matrix:
     def __init__(self, matrix_string):
         rows = matrix_string.splitlines()
-        self.matrix = [[int(num) for num in row.split(" ")] for row in rows]
+        self.matrix = [[int(num) for num in row.split()] for row in rows]
 
     def row(self, index):
         return self.matrix[index - 1]
 
     def column(self, index):
-        column = []
-        for row in self.matrix:
-        	column.append(row[index - 1])
-        return column
+        return [row[index - 1] for row in self.matrix]
