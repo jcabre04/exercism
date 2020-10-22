46ca18fc5a5fdd35d30bd2ae4e5859a7ed917155
Jose Cabrera Flores <cabrera.student@gmail.com>
2020-10-22 14:55:30 -0700

    Condense Isogram logic into 1 line of code

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,14 +1,3 @@
-def _loop_with_set(string):
-	charExists = set()
-
-	for char in string:
-		if char in charExists:
-			return False
-		else:
-			charExists.add(char)
-
-	return True
-
 def is_isogram(string):
-	string = string.replace("-", "").replace(" ", "").lower()
-	return _loop_with_set(string)
+	clean_string = string.replace("-", "").replace(" ", "").lower()
+	return len(clean_string) == len(set(clean_string))
